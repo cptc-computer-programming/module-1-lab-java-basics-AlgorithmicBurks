caa5792db69a15f5529c58e5014f263151b1fa33
Mark Burks Jr. <burksmarkjr20@gmail.com>
2026-07-14 11:32:17 -0700

    OnlineOrder.java
    took out todo

--- src/main/java/week01/part02_methods/OnlineOrder.java
+++ src/main/java/week01/part02_methods/OnlineOrder.java
@@ -7,17 +7,6 @@ public class OnlineOrder {
         processOrder();
     }
 
-    // TODO: main must call this method.
-    // This method must call the other methods in this exact order:
-    //   1) addItems()
-    //   2) checkout()
-    //   3) confirmOrder()
-    //
-    // It must print exactly:
-    // Processing order...
-    // Adding items to cart.
-    // Checking out.
-    // Order confirmed.
     public static void processOrder() {
         System.out.println("Processing order...");
         addItems();
@@ -25,17 +14,17 @@ public class OnlineOrder {
         confirmOrder();
     }
 
-    // TODO: Print exactly: Adding items to cart.
+    
     public static void addItems() {
         System.out.println("Adding items to cart.");
     }
 
-    // TODO: Print exactly: Checking out.
+   
     public static void checkout() {
         System.out.println("Checking out.");
     }
 
-    // TODO: Print exactly: Order confirmed.
+   
     public static void confirmOrder() {
         System.out.println("Order confirmed.");
     }
